0190f1d5148c898dc968a3f5081c1fac76a544b0
autthapol-k <polcpekusrc@gmail.com>
2021-03-31 11:11:31 +0700

    add ios clear session into plugin

--- aws_session/ios/Classes/SwiftAwsSessionPlugin.swift
+++ aws_session/ios/Classes/SwiftAwsSessionPlugin.swift
@@ -1,14 +1,26 @@
 import Flutter
 import UIKit
+import AWSCognitoIdentityProvider
 
 public class SwiftAwsSessionPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "aws_session", binaryMessenger: registrar.messenger())
-    let instance = SwiftAwsSessionPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "aws_session", binaryMessenger: registrar.messenger())
+        let instance = SwiftAwsSessionPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard call.method == "clearSession" else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        
+        self.clearSession(result: result)
+    }
+    
+    private func clearSession(result: FlutterResult) {
+        AWSCognitoIdentityUserPool.default().currentUser()?.clearSession()
+        
+        result(true)
+    }
 }
